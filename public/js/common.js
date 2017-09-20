8d4d4cfee378401d42f224ec051b668807a477ab
Hot-pink <stefaniejay@sohu.com>
2017-09-20 13:40:14 +0800

    模板引擎渲染头像

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function(){
+define(['jquery','template','cookie'],function($,template){
     // NProgress.start();
     // NProgress.done();
 
@@ -26,8 +26,12 @@ define(['jquery','cookie'],function(){
     //根据cookie传的参数，设置用户头像
     var logInfo =$.cookie("logInfo");
     logInfo =logInfo && JSON.parse(logInfo);
-    $('.aside .avatar img').attr({
-        "src": logInfo.tc_avatar
-    });
-    $('.aside h4').html(logInfo.tc_name)
+    // $('.aside .avatar img').attr({
+    //     "src": logInfo.tc_avatar
+    // });
+    // $('.aside h4').html(logInfo.tc_name)
+    var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+    var html = template.render(tpl,logInfo);
+    $('.aside .profile').html(html);
+
 })
