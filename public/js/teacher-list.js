4863db45a47944ba30e0d78eb7539d232d856ade
Hot-pink <stefaniejay@sohu.com>
2017-09-20 14:37:55 +0800

    教师列表完成

---
+++ public/js/teacher-list.js
@@ -0,0 +1,11 @@
+define(['jquery','template'],function($,template){
+    $.ajax({
+        type:'get',
+        url:'/api/teacher',
+        success:function(data){
+            console.log(data);
+            var html = template('teacherTpl',data);
+            $('#teacherInfo').html(html);
+        }
+    })
+})
