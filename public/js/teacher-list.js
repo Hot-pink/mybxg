1e85cec2948d95058feaea6a2c8da98300c26386
Hot-pink <stefaniejay@sohu.com>
2017-09-21 20:23:23 +0800

    完成讲师注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -3,9 +3,32 @@ define(['jquery','template'],function($,template){
         type:'get',
         url:'/api/teacher',
         success:function(data){
-            console.log(data);
             var html = template('teacherTpl',data);
             $('#teacherInfo').html(html);
+
+            // 启用注销功能
+            $('.eod').click(function () {
+                var $that = $(this);
+                var $td = $(this).closest("td");
+                var tcId = $td.attr("data-tcId");
+                var tcStatus = $td.attr("data-status");
+                $.ajax({
+                    type:'post',
+                    url:'/api/teacher/handle',
+                    datatype:'json',
+                    data:{tc_id:tcId,tc_status:tcStatus},
+                    success: function (data) {
+                        if(data.code==200){
+                            $td.attr("data-status",data.result.tc_status)
+                            if(data.result.tc_status == 1){
+                                $that.html("启用");
+                            }else{
+                                $that.html("注销");
+                            }
+                        }
+                    }
+                })
+            })
         }
-    })
+    });
 })
