5cc961b934d4a88b3605196b0f84ef87f005471f
Hot-pink <stefaniejay@sohu.com>
2017-09-22 09:15:41 +0800

    完成添加编辑讲师功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'], function ($, template,util) {
+define(['jquery','template','util','datepicker','language'], function ($, template,util) {
     //调用封装的工具函数获取url里的get提交的id参数
     var tcId = util.qs("tc_id");
     if(tcId){
@@ -10,12 +10,33 @@ define(['jquery','template','util'], function ($, template,util) {
             success: function (data) {
                 var html = template('teacherTpl',data.result)
                 $('#teacherInfo').html(html);
+                submitForm('/api/teacher/update')
             }
         })
     }else{
         //没有id添加操作
         var html = template('teacherTpl',{})
         $('#teacherInfo').html(html);
+        submitForm('/api//teacher/add');
+    }
+
+    //封装公用提交函数
+    function submitForm(url){
+        $('.btn-sm').click(function () {
+            $.ajax({
+                type:'post',
+                url:url,
+                data:$('#tecInfo').serialize(),
+                datatype:'json',
+                success: function (data) {
+                    console.log(data);
+                    if(data.code == 200){
+                        alert("提交成功");
+                        location.href="/teacher/list";
+                    }
+                }
+            })
+        })
 
     }
 })
