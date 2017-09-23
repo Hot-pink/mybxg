d56efc49db1359e369d65620efb315d2089d4d4d
Hot-pink <stefaniejay@sohu.com>
2017-09-23 19:18:11 +0800

    完成头像上传功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,11 +1,26 @@
-define(['jquery','template'], function ($,template) {
+define(['jquery','template','uploadify'], function ($,template) {
     $.ajax({
         url:'/api/teacher/profile',
         datatype:'json',
         success: function (data) {
-            console.log(data);
             var html = template("settingTpl",data.result)
             $('#formInfo').html(html);
+
+            //上传文件
+            $('#upfile').uploadify({
+                width:120,
+                height:120,
+                buttonText:'',
+                itemTemplate:'<span></span>',
+                swf:'/public/assets/uploadify/uploadify.swf',
+                //后台接口位置
+                uploader:'/api/uploader/avatar',
+                fileObjName:'tc_avatar',
+                onUploadSuccess: function (a, b, c) {
+                    var obj = JSON.parse(b);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            })
         }
     })
 })
