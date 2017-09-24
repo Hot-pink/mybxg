99c916b954da96d4c74d773027f5fc14e87358e8
Hot-pink <stefaniejay@sohu.com>
2017-09-24 20:13:37 +0800

    添加副文本功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,5 @@
-define(['jquery','template','uploadify','region'], function ($,template) {
+define(['jquery','template','ckeditor','uploadify','region',,'datepicker','language'], function ($,template) {
+    //渲染信息中心页面
     $.ajax({
         url:'/api/teacher/profile',
         datatype:'json',
@@ -17,6 +18,7 @@ define(['jquery','template','uploadify','region'], function ($,template) {
                 uploader:'/api/uploader/avatar',
                 fileObjName:'tc_avatar',
                 onUploadSuccess: function (a, b, c) {
+                    console.log(b);
                     var obj = JSON.parse(b);
                     $('.preview img').attr('src',obj.result.path);
                 }
@@ -25,6 +27,16 @@ define(['jquery','template','uploadify','region'], function ($,template) {
             //省市县三级联动
             $('#pcd').region({
                 url:'/public/assets/jquery-region/region.json'
+            })
+            
+            //添加副文本功能
+            CKEDITOR.replace('editor',{
+                toolbarGroups : [
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                    { name: 'links', groups: [ 'links' ] },
+                    { name: 'insert', groups: [ 'insert' ] }
+                ]
             })
         }
     })
